a751e0ac0a7c7585640c9387a5a32b944fd366d7
Luis <211g0373@rcarbonifera.tecnm.mx>
2025-09-28 20:54:43 -0600

    nodo

---
+++ NumberPuzzle_Team_3/Models/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPuzzle_Team_3.Models
+{
+    public class Nodo
+    {
+        string estado;
+        Nodo? Padre;
+        int costo;
+        int heuristica;
+        int pasos;
+
+
+    }
+}
--- NumberPuzzle_Team_3/Program.cs
+++ NumberPuzzle_Team_3/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using NumberPuzzle_Team_3.Models;
+
+
+
+
+
+
+
+Nodo nodo = new Nodo();
+
 Console.WriteLine("Hello, World!");
